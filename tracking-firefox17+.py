a8fa2a5cac8ece820944d303f7c574722b1a5e79
Lianne <lianne@lianne-VirtualBox.(none)>
2012-12-07 22:14:46 +1100

    PST time considered

--- tracking-firefox17+.py
+++ tracking-firefox17+.py
@@ -13,20 +13,25 @@ options = {
     'changed_after':    start.strftime('%Y-%m-%d'),
     'changed_before':   end.strftime('%Y-%m-%d'),
     'changed_field':    'cf_tracking_firefox17',
-    'changed_from':     '+',
-    'include_fields':   '_default,attachments,history',
     'field0-0-0':       'cf_status_firefox17',
     'type0-0-0':        'not_equals',
-    'value0-0-0':       'fixed',
+    'value0-0-0':       'disabled',
     'field0-1-0':       'cf_status_firefox17',
     'type0-1-0':        'not_equals',
-    'value0-1-0':       'verified',
-    'field0-2-0':       'cf_status_firefox17',
-    'type0-2-0':        'not_equals',
-    'value0-2-0':       'disabled',
-    'field0-3-0':       'cf_status_firefox17',
-    'type0-3-0':        'not_equals',
-    'value0-3-0':       'unaffected',
+    'value0-1-0':       'unaffected',
+    #'field0-2-0':       'cf_status_firefox17',
+    #'type0-2-0':        'not_equals',
+    #'value0-2-0':       'verified',
+    'field0-2-0':       'cf_tracking_firefox17',
+    'type0-2-0':        'equals',
+    'value0-2-0':       '+',
+    #'field0-3-0':       'cf_status_firefox17',
+    #'type0-3-0':        'not_equals',
+    #'value0-3-0':       'wontfix',
+    #'field0-3-0':       'cf_status_firefox17',
+    #'type0-3-0':        'not_equals',
+    #'value0-3-0':       'fixed',
+    'include_fields':   '_default,attachments,history',
 }
 
 buglist = bmo.get_bug_list(options)
@@ -40,16 +45,15 @@ removed = [0]*(period)
 fixed = [0]*(period)
 new = 0
 total = len(buglist)
+fixed_bugs = []
 
 for bug in buglist:
     related = True
-    
-    if bug.status == 'NEW':
-        new = new + 1
     for changeset in bug.history:
-        mod_date = date(changeset.change_time.year,changeset.change_time.month,changeset.change_time.day)
+        mod_time = changeset.change_time - timedelta(hours=8)
+        mod_date = date(mod_time.year,mod_time.month,mod_time.day)
         index = (mod_date-start).days
-        if index >= 0 and index < 42:
+        if index >= 0 and index < period:
             for change in changeset.changes:
                 if change.field_name == 'cf_tracking_firefox17':
                     if change.added == '+':
@@ -57,16 +61,24 @@ for bug in buglist:
                     elif change.removed == '+':
                         removed[index] = removed[index] + 1
                 elif change.field_name == 'status':
-                    if change.added == 'RESOLVED':
-                        fixed[index] = fixed[index] + 1
-                    if change.added == 'REOPENED':
-                        fixed[index] = fixed[index] - 1
-                elif change.field_name == 'cf_status_firefox17':
                     print change.added
-                    if change.added == 'wontfix' or change.added == 'fixed':
+                    if (change.added == 'RESOLVED' or change.added == 'VERIFIED') and bug not in fixed_bugs:
                         fixed[index] = fixed[index] + 1
+                        fixed_bugs.append(bug)
+                    if change.added == 'REOPENED' and bug in fixed_bugs:
+                        fixed[index] = fixed[index] - 1
+                        fixed_bugs.remove(bug)
+                elif change.field_name == 'cf_status_firefox17':
+                    if (change.added == 'fixed' or change.added == 'wontfix' or change.added == 'verified') and bug not in fixed_bugs:
+                        print change.added
+                        fixed[index] = fixed[index] + 1
+                        fixed_bugs.append(bug)
 
-print new
+for bug in fixed_bugs:
+    if bug in buglist:
+        buglist.remove(bug)
+        
+print buglist
                 
 f = open('results.txt', 'w')
 f.write('date,added,removed,closed,total remaining\n')
